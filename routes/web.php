411d18849d27f7f6f4c45cd33463d025c9e14703
Popsana Barida <popsyjunior@gmail.com>
2026-01-20 20:23:15 +0100

    chore: remove ui elements

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Laravel\Fortify\Features;
 
 Route::get('/', function () {
     return response('OK', 200);
 })->name('home');
-
-require __DIR__.'/settings.php';
